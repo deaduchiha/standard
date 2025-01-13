a2d162039e4cf8f50387886c34995f81cb14ffb4
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-01-13 22:27:39 +0330

    chore setup fonts and install ofetch

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,8 +1,54 @@
-import { Vazirmatn } from "next/font/google";
+import localFont from "next/font/local";
 
-const vazir = Vazirmatn({
-  subsets: ["arabic"],
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
+const vazirmatn = localFont({
+  src: [
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/vazirmatn/Vazirmatn-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-vazirmatn",
 });
 
-export { vazir };
+export { vazirmatn };
