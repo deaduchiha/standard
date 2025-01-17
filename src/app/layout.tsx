94bfdde0207a2b414d550cc11a3ae0f71fd6544f
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-01-18 00:34:40 +0330

    fix max scale zoom iphone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      ></meta>
       <body className={`${vazirmatn.className} ${vazirmatn.variable}`}>
         <Providers>{children}</Providers>
         <Toaster richColors className="font-vazir" theme="light" />
